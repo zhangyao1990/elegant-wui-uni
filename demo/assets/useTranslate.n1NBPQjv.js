ad9f35fd044e3128d68d9198f96d5c457cf2304f
zhangyao <97972619@qq.com>
2025-02-17 14:42:09 +0800

    deploy

---
+++ demo/assets/useTranslate.n1NBPQjv.js
@@ -0,0 +1,1 @@
+import{B as s,L as a,C as r,D as t}from"./page-wraper.BMayt547.js";const e=e=>{const o=e?s(e)+".":"";return{translate:(s,...e)=>{const n=a.messages(),p=r(n,o+s);return t(p)?p(...e):p}}};export{e as u};

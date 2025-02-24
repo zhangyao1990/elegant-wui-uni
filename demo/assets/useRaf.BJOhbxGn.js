9f7ca6e9f27b4c3d6dc3eb13c241232b31290698
zhangyao <97972619@qq.com>
2025-02-24 09:49:13 +0800

    deploy

--- demo/assets/useRaf.BJOhbxGn.js
+++
@@ -1,1 +0,0 @@
-import{r as a,Y as e}from"./index-72jEY8AL.js";import{r,a1 as t,n}from"./page-wraper.DpqTXTZc.js";function o(o){const s=a(null),u=()=>{r&&t(s.value)?cancelAnimationFrame(s.value):n(s.value)&&clearTimeout(s.value)};return e((()=>{u()})),{start:()=>{const a=a=>{o(a)};s.value=r?requestAnimationFrame(a):setTimeout((()=>a(Date.now())),1e3/30)},cancel:u}}export{o as u};

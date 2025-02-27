6d980ed721bc0ce42da1bd8eb567d07b0554bc3b
zhangyao <97972619@qq.com>
2025-02-27 09:14:19 +0800

    feat: ✨ waterFall组件图片替换，方便微信小程序提审

--- src/pages-data/waterFall/data/image-data.ts
+++ src/pages-data/waterFall/data/image-data.ts
@@ -3,6 +3,7 @@ import { onReachBottom } from '@dcloudio/uni-app'
 
 export const useGenerateImageData = () => {
   const images = [
+
     'https://gips3.baidu.com/it/u=1821127123,1149655687&fm=3028&app=3028&f=JPEG&fmt=auto?w=720&h=1280',
     'https://gips0.baidu.com/it/u=1490237218,4115737545&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=720',
     'https://gips1.baidu.com/it/u=1647344915,1746921568&fm=3028&app=3028&f=JPEG&fmt=auto?w=720&h=1280',
@@ -10,8 +11,8 @@ export const useGenerateImageData = () => {
     'https://gips1.baidu.com/it/u=2205169440,1005663887&fm=3028&app=3028&f=JPEG&fmt=auto?w=1024&h=1024',
     'https://gips1.baidu.com/it/u=1239311027,2819893885&fm=3028&app=3028&f=JPEG&fmt=auto?w=1024&h=1024',
     'https://gips2.baidu.com/it/u=4231193786,3187314859&fm=3028&app=3028&f=JPEG&fmt=auto?w=1024&h=1024',
-    'https://gips3.baidu.com/it/u=3886271102,3123389489&fm=3028&app=3028&f=JPEG&fmt=auto?w=1280&h=960',
-    'https://gips2.baidu.com/it/u=3944689179,983354166&fm=3028&app=3028&f=JPEG&fmt=auto?w=1024&h=1024',
+    'https://gips0.baidu.com/it/u=3602773692,1512483864&fm=3028&app=3028&f=JPEG&fmt=auto?w=960&h=1280',
+    'https://gips0.baidu.com/it/u=3560029307,576412274&fm=3028&app=3028&f=JPEG&fmt=auto?w=960&h=1280',
     'https://gips3.baidu.com/it/u=1874299413,3253595329&fm=3028&app=3028&f=JPEG&fmt=auto?w=1920&h=2560',
   ]
 

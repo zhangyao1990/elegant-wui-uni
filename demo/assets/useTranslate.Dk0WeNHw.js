acc25c8c19114e9cd172c1008041405fe4a04327
zhangyao <97972619@qq.com>
2025-02-28 13:25:56 +0800

    deploy

---
+++ demo/assets/useTranslate.Dk0WeNHw.js
@@ -0,0 +1,1 @@
+import{G as s,L as r,H as a,D as t}from"./page-wraper.BAAFjrch.js";const e=e=>{const n=e?s(e)+".":"";return{translate:(s,...e)=>{const o=r.messages(),p=a(o,n+s);return t(p)?p(...e):p}}};export{e as u};

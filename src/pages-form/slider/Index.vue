9d7f115b49358d2730586c1c2c640efa8cc8d0a2
zhangyao <97972619@qq.com>
2025-07-15 09:13:21 +0800

    fix: 🐛 修复 Slider 处理边界值异常的问题，优化样式和事件处理逻辑

--- src/pages-form/slider/Index.vue
+++ src/pages-form/slider/Index.vue
@@ -4,7 +4,7 @@
       <wui-slider v-model="value1" />
     </demo-block>
     <demo-block title="指定选择范围">
-      <wui-slider v-model="value2" :min="-10" :max="10" />
+      <wui-slider v-model="value8" :min="160" :max="280" :step="30" />
     </demo-block>
     <demo-block title="指定步长">
       <wui-slider v-model="value4" hide-min-max :step="10" />
@@ -29,6 +29,7 @@ const value4 = ref<number>(11)
 const value5 = ref<number>(70)
 const value6 = ref<number[]>([20, 40])
 const value7 = ref<number>(20)
+const value8 = ref<number>(185)
 </script>
 
 <style lang="css" scoped></style>

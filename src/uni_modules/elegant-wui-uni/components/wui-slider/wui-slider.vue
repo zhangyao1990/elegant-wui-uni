21baeadd0154d6ab1f61923420b7272156ebbaa3
zhangyao <97972619@qq.com>
2025-07-16 13:19:08 +0800

    fix: 🐛 修复 wui-slider 滑块不跟手的问题、新增自定义滑块按钮

--- src/uni_modules/elegant-wui-uni/components/wui-slider/wui-slider.vue
+++ src/uni_modules/elegant-wui-uni/components/wui-slider/wui-slider.vue
@@ -1,10 +1,10 @@
 <template>
-  <view :class="rootClass" :style="customStyle" :id="sliderId">
+  <view :class="rootClass" :style="customStyle">
     <!-- #ifdef MP-DINGTALK -->
-    <view :id="sliderId" style="flex: 1" :class="rootClass">
+    <view style="flex: 1" :class="rootClass">
       <!-- #endif -->
       <view :class="`wui-slider__label-min ${customMinClass}`" v-if="!hideMinMax">{{ minProp }}</view>
-      <view class="wui-slider__bar-wrapper" :style="wrapperStyle">
+      <view class="wui-slider__bar-wrapper" :style="wrapperStyle" :id="sliderBarWrapperId">
         <view class="wui-slider__bar" :style="barStyle">
           <template v-if="isRange">
             <!-- 左边滑块 -->
@@ -17,7 +17,9 @@
               @touchcancel="onSliderTouchEnd"
             >
               <view class="wui-slider__label" v-if="!hideLabel">{{ firstValue }}</view>
-              <view class="wui-slider__button" />
+              <view class="wui-slider__button">
+                <slot name="button"></slot>
+              </view>
             </view>
             <!-- 右边滑块 -->
             <view
@@ -76,7 +78,7 @@ const props = defineProps(sliderProps)
 const emit = defineEmits<SliderEmits>()
 
 // ----------- 基础状态 -----------
-const sliderId = ref<string>(`sliderId${uuid()}`)
+const sliderBarWrapperId = ref<string>(`sliderBarWrapperId${uuid()}`)
 const touch = useTouch()
 const touchIndex = ref<number>(0)
 const { proxy } = getCurrentInstance() as any
@@ -232,7 +234,7 @@ function clamp(value: number, min: number, max: number): number {
  * 初始化滑块宽度
  */
 function initSlider() {
-  getRect(`#${sliderId.value}`, false, proxy).then((data) => {
+  getRect(`#${sliderBarWrapperId.value}`, false, proxy).then((data) => {
     trackWidth.value = Number(data.width)
     trackLeft.value = Number(data.left)
   })
